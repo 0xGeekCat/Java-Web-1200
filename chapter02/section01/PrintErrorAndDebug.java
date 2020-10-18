e066d8d80160397eb56a5196c6fd7e3a5cdd9ed3
0xGeekCat <1292881925@qq.com>
2020-10-18 11:09:25 +0800

    《Java Web开发实战1200例》学习代码集

---
+++ chapter02/section01/PrintErrorAndDebug.java
@@ -0,0 +1,11 @@
+//package JavaWeb1200.chapter02.section01;
+//
+//public class PrintErrorAndDebug {
+//    public static void main(String[] args) {
+//        System.out.println("main()方法开始运行");
+//        System.err.println("在运行期间手动输出一个错误信息: ");
+//        System.err.println("\t 注意安全");
+//        System.out.println("PrintErrorAndDebug.main()");
+//        System.out.println("main()方法运行结束");
+//    }
+//}
